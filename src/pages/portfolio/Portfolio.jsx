5be177321d2fb5c0a3da2c87e1ae904eacfbaa7e
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-17 21:18:12 +0530

    pb

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PortfolioCards from '../../components/PortfolioCards'
+import Mochten from '../../components/Mochten'
 
 const Portfolio = () => {
   return (
     <div>
       <PortfolioCards/>
+      <Mochten/>
     </div>
   )
 }
