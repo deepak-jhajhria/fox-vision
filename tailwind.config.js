f0c54523ee83a6dfc5d2585d767f3ca34ef8c91c
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-17 09:45:53 +0530

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        blue: "#4B17E6",
+        blackPearl: "#071838",
+        turquoise: "#05D5FB",
+        bubbles: "#E9FCFF",
+        blueChalk: "#E6DEFF",
+      },
+      lineHeight: {
+        '1.5': '1.5',
+      },
+      fontSize: {
+      },
+      backgroundImage: {
+      },
+      boxShadow: {
+
+      },
+    },
   },
   plugins: [],
 }
