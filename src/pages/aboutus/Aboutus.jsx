abbabb31f0b0b67c6f0225a3080575f9e2b571a5
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-06-17 21:11:51 +0530

    make about us

---
+++ src/pages/aboutus/Aboutus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Customer from '../../components/Customer'
+
+const Aboutus = () => {
+  return (
+    <>
+    <Customer/>
+    </>
+  )
+}
+
+export default Aboutus
