14d93c93b38ebf66fb5d0325ff191e81bb1a5b41
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-17 14:05:48 +0530

    merge from dev

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import Portfolio from './pages/portfolio/Portfolio';
@@ -5,8 +6,10 @@ import Portfolio from './pages/portfolio/Portfolio';
 function App() {
   return (
     <div className='max-w-[1920px] mx-auto'>
-      <Homepage />
-      {/* <Portfolio/> */}
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
