6b2ca4e0a63470b543a98c4dfa15006c4223c5f2
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-19 10:18:32 +0530

    update homepage

--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -29,7 +29,7 @@ const ScrollToTop = () => {
     };
 
     return (
-        <div className={`border-0 fixed bottom-4 right-4 transition-all duration-300 ${showScrollTop ? "scale-100 translate-y-0" : "scale-0 translate-y-[40px]"}`}>
+        <div className={`border-0 fixed z-50 bottom-4 right-4 transition-all duration-300 ${showScrollTop ? "scale-100 translate-y-0" : "scale-0 translate-y-[40px]"}`}>
             <div onClick={scrollTopHandler} className="flex items-center justify-center overflow-hidden transition-all duration-500 rounded-full cursor-pointer h-9 w-9 sm:h-10 bg-skyBlue sm:w-10 bg-dodgerBlue hover:shadow-lg hover:shadow-sky-300">
                 <BackToTop />
             </div>
