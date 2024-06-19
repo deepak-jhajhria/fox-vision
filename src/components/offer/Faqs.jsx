4dd681cef84fab000dc6246fab0da0acc746abe3
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-19 12:17:54 +0530

    faqs update

--- src/components/offer/Faqs.jsx
+++ src/components/offer/Faqs.jsx
@@ -4,9 +4,9 @@ import Accordion from "./Accordion"
 
 const Faqs = () => {
     return (
-        <section className="px-6 py-[150px] mb-[144px]">
+        <section className="px-4 sm:px-6 py-[150px] mb-[144px]">
             <div className="container max-w-[970px]">
-                <H2 heading='Frequently Asked Questions' />
+                <H2 className='text-center' heading='Frequently Asked Questions' />
                 <Accordion className='mt-[50px]' data={accordionList}/>
             </div>
         </section>
