4692691f5123ebd3200c054eeac56fb6cea2593e
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-19 16:48:14 +0530

    button hover

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
   const { content, className } = props;
   return (
-    <button className={`rounded-[69px] bg-turquoise py-3.5 px-[30px] text-white font-poppins text-base font-semibold !leading-150 ${className}`}> {content}</button>
+    <button className={`rounded-[69px] relative overflow-hidden after:absolute after:top-0 hover:after:right-0 after:bottom-0 after:left-0 after:right-[100%] after:duration-300 after:bg-blue after:z-[-1] z-[1] bg-turquoise py-3.5 px-[30px] text-white after:h-full font-poppins text-base font-semibold !leading-150 ${className}`}> {content}</button>
   );
 };
 
