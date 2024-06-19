7d5871a444fe2fee5d3be9b0ef23ff8e6893f72d
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-06-19 15:48:00 +0530

    merge from main

--- src/components/aboutus/DesignMastery.jsx
+++ src/components/aboutus/DesignMastery.jsx
@@ -15,7 +15,10 @@ const DesignMastery = () => {
         />
         <div className="flex flex-col md:flex-row gap-5 justify-between items-center bg-transparent overflow-hidden shadow-[0px_-51px_68px_-51px_#00000012] p-5 md:p-6 lg:p-[35px]">
           <div className="flex flex-col">
-            <H4 className="text-blackPearl mb-[15px]" heading="Mhill Paloka" />
+            <H4
+              className="text-blackPearl mb-2 md:mb-[15px]"
+              heading="Mhill Paloka"
+            />
             <H5
               className="text-blackPearl !leading-normal lg:!leading-183 !font-normal md:max-w-[500px] opacity-70"
               title="Mhill Paloka deepened his know-how in the areas of purchasing, marketing and controlling in a large international company. He has also been a founding member of a facility management company since 2009. Design has always been extremely important to him and he is happy to help you stay up to date."
