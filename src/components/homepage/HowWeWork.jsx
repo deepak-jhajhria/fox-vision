d4b58589cc586da2c3af5d84c85d555d7f50fe3b
Priyanka-390 <psr82220@gmail.com>
2024-06-20 12:09:23 +0530

    remove padding resultdriven

--- src/components/homepage/HowWeWork.jsx
+++ src/components/homepage/HowWeWork.jsx
@@ -7,7 +7,7 @@ const HowWeWork = ({ imgClassName,margin,padding }) => {
     <div className="px-6 relative z-[1] sm:pt-12 pt-10 xl:pt-36 lg:pt-24 md:pt-16 pb-[3px]">
       <div className="container max-w-[1164px]">
         <ResultsDriven margin={margin} imgClassName={imgClassName} />
-        <div className={`flex flex-row flex-wrap justify-center py-6 -mx-3 xl:py-28 lg:py-24 sm:py-8 md:py-16 xl:my-3 ${padding}`}>
+        <div className={`flex flex-row flex-wrap justify-center py-6 -mx-3 xl:py-28 lg:py-24 sm:py-8 max-sm:mb-4 md:py-16 xl:my-3 ${padding}`}>
           {howWeWorkList.map((obj, i) => (
             <div key={i} className="flex justify-center w-full px-2 lg:w-1/3 sm:w-1/2">
               <WorkCard icons={obj.icons} tittle={obj.tittle} />
