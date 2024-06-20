afcf20a330b0ff52cbb79c113d6a2e240659d26e
sachin-niyana <sachinjakhar100@gmail.com>
2024-06-20 12:36:24 +0530

    merge cide

--- src/components/portfolio/PortfolioCards.jsx
+++ src/components/portfolio/PortfolioCards.jsx
@@ -4,9 +4,9 @@ import { portCardData } from '../common/Helper'
 
 const PortfolioCards = () => {
     return (
-        <div className='xl:pt-[123px] lg:pt-24 md:pt-16 sm:pt-12 pt-10 pb-[185px] md:pb-[227px] xl:pb-[293px] sm:px-6 px-3'>
+        <div className='xl:pt-[123px] lg:pt-24 md:pt-16 sm:pt-12 pt-10 pb-[160px] sm:pb-[170px] md:pb-[180px] lg:pb-[190px] xl:pb-[293px] sm:px-6 px-3'>
             <div className='container max-w-[1140px]'>
-                <div className='flex flex-row flex-wrap -mx-3 mb-4 md:mb-[26px] justify-center  lg:justify-normal'>
+                <div className='flex flex-row flex-wrap -mx-3 md:mb-2 xl:mb-[26px] justify-center  lg:justify-normal'>
                     {portCardData.map((item, i) => (
                         <div className={`lg:w-1/3 sm:w-1/2 w-full px-3 flex justify-center ${1 <= 9 ? "mb-6" : "mb-0"}`}>
                             <PortfolioCardLayout index={i} img={item.img} head={item.head} discription={item.discription} />
