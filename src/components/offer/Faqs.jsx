d2680872e07e8d6b49b5a96894ab5233db8d8ee9
Priyanka-390 <psr82220@gmail.com>
2024-06-20 14:20:01 +0530

    section around padding

--- src/components/offer/Faqs.jsx
+++ src/components/offer/Faqs.jsx
@@ -4,7 +4,7 @@ import Accordion from "./Accordion"
 
 const Faqs = () => {
     return (
-        <section className="px-4 sm:px-6 py-10 sm:py-12 md:py-16 xl:pb-[150px] xl:pt-[134px] lg:py-24 mb-[144px]">
+        <section className="px-3 sm:px-6 py-10 sm:py-12 md:py-16 xl:pb-[150px] xl:pt-[134px] lg:py-24 mb-[144px]">
             <div className="container max-w-[970px]">
                 <H2 className='text-center' heading='Frequently Asked Questions' />
                 <Accordion className='mt-6 sm:mt-8 lg:mt-[50px]' data={accordionList}/>
