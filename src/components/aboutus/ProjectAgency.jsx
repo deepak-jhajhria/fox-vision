cd0b49dac38d819a511bd353226e7bae7a06377a
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-06-20 14:16:13 +0530

    merge from main

--- src/components/aboutus/ProjectAgency.jsx
+++ src/components/aboutus/ProjectAgency.jsx
@@ -5,48 +5,50 @@ import { ProjectArrow, WhatWeSecLogo, WhatWeSecVector } from "../common/Icons";
 
 const ProjectAgency = () => {
   return (
-    <section className="py-10 sm:py-12 md:py-16 lg:py-24 xl:pb-0 xl:pt-[126px] px-6 overflow-hidden">
-      <div className="container px-0 max-w-[1140px] relative">
-        <H5 className="uppercase max-md:text-center" title="FOX VISION" />
-        <H2
-          className="mt-1 md:mt-2 !leading-120 max-md:text-center max-md:mx-auto md:max-w-[440px] lg:max-w-[498px]"
-          heading="Design Project Agency"
-        />
-        <div className="flex items-center max-md:flex-col max-sm:gap-3 max-lg:gap-5 xl:items-end ">
-          <img
-            className="w-full max-w-[380px] md:max-w-[400px] lg:max-w-[524px] mt-4 sm:mt-0 lg:mt-[29px] xl:max-w-[590px] lg:h-[328px] h-full"
-            src={cardImage}
-            alt="cardimage"
+    <section>
+      <div className="py-10 sm:py-12 md:py-16 lg:py-24 xl:pb-0 xl:pt-[126px] px-6 overflow-hidden">
+        <div className="container px-0 max-w-[1140px] relative">
+          <H5 className="uppercase max-md:text-center" title="FOX VISION" />
+          <H2
+            className="mt-1 md:mt-2 !leading-120 max-md:text-center max-md:mx-auto md:max-w-[440px] lg:max-w-[498px]"
+            heading="Design Project Agency"
           />
-          <div className="flex relative flex-col items-start md:translate-y-[-16%] lg:ml-8 ">
-            <Description
-              className="max-md:text-center max-sm:mt-28 lg:max-w-[523px]"
-              description={
-                <>
-                  
-                  As a design agency, we create a contemporary and
-                  {
-                    <span className="text-turquoise"> distinctive appearance </span>
-                  }
-                  for you. Style is more in demand than ever. We help you show
-                  your best side to your customers. you
-                  {<span className="text-turquoise"> Strong branding sets </span>}
-                  apart from your competition and creates trust and seriousness.
-                </>
-              }
+          <div className="flex items-center max-md:flex-col max-sm:gap-3 max-lg:gap-5 xl:items-end ">
+            <img
+              className="w-full max-w-[380px] md:max-w-[400px] lg:max-w-[524px] mt-4 sm:mt-0 lg:mt-[29px] xl:max-w-[590px] lg:h-[328px] h-full"
+              src={cardImage}
+              alt="cardimage"
             />
-            <CommonButton
-              className="mt-8 max-md:mx-auto md:mt-10"
-              name="Contact"
-            />
-            <span className="absolute right-[40%] sm:right-4 top-0 sm:-top-24">
-              <WhatWeSecLogo />
-            </span>
+            <div className="flex relative flex-col items-start md:translate-y-[-16%] lg:ml-8 ">
+              <Description
+                className="max-md:text-center max-sm:mt-28 lg:max-w-[523px]"
+                description={
+                  <>
+      
+                    As a design agency, we create a contemporary and
+                    {
+                      <span className="text-turquoise"> distinctive appearance </span>
+                    }
+                    for you. Style is more in demand than ever. We help you show
+                    your best side to your customers. you
+                    {<span className="text-turquoise"> Strong branding sets </span>}
+                    apart from your competition and creates trust and seriousness.
+                  </>
+                }
+              />
+              <CommonButton
+                className="mt-8 max-md:mx-auto md:mt-10"
+                name="Contact"
+              />
+              <span className="absolute right-[40%] sm:right-4 top-0 sm:-top-24">
+                <WhatWeSecLogo />
+              </span>
+            </div>
           </div>
+          <span className="absolute top-[52%] sm:top-[44%] md:top-0 lg:left-[39%] md:left-[39%] lg:top-10 xl:top-[81px] -left-1 xl:left-[42%]">
+            <ProjectArrow />
+          </span>
         </div>
-        <span className="absolute top-[52%] sm:top-[44%] md:top-0 lg:left-[39%] md:left-[39%] lg:top-10 xl:top-[81px] -left-1 xl:left-[42%]">
-          <ProjectArrow />
-        </span>
       </div>
     </section>
   );
--- src/components/homepage/Angebot.jsx
+++ src/components/homepage/Angebot.jsx
@@ -6,41 +6,42 @@ import { howWeWorkCardlist } from "../common/Helper";
 
 const Angebot = () => {
   return (
-    <div className="bg-blue px-3 sm:px-6 pt-10 -mt-[1px] md:pt-16 sm:pt-12 lg:pt-24 xl:pt-[122px] pb-36 md:pb-[170px] lg:pb-[190px] xl:pb-[238px]">
-      <div className="container max-w-[1164px] mx-auto">
-        <H5
-          className="!text-white  text-center mb-2 md:mb-3"
-          title="A RESULTS-DRIVEN AGENCY"
-        />
-        <H2
-          className="!text-white  text-center mb-2 sm:mb-5 lg:mb-6 xl:mb-[76px]"
-          heading="Angebot"
-        />
-
-        <div className="flex flex-row -mx-3 pb-6 md:pb-16 lg:pb-[77px] justify-center flex-wrap ">
-          {howWeWorkCardlist.map((obj, index) => (
-            <div
-              className="lg:w-4/12 md:1/2 pt-4 md:pt-[22px] !px-[11.5px] sm:w-[49%] w-full"
-              key={index}
-            >
-              <Corporate
-                svg={obj.svg}
-                title={obj.title}
-                description={obj.description}
-                linkText={obj.link}
-                arrow={<Arrow />}
-                cardBox={index === 1 || index === 3 || index === 5}
-              />
-            </div>
-          ))}
+    <section>
+      <div className="bg-blue px-3 sm:px-6 pt-10 -mt-[1px] md:pt-16 sm:pt-12 lg:pt-24 xl:pt-[122px] pb-36 md:pb-[170px] lg:pb-[190px] xl:pb-[238px]">
+        <div className="container max-w-[1164px] mx-auto">
+          <H5
+            className="!text-white  text-center mb-2 md:mb-3"
+            title="A RESULTS-DRIVEN AGENCY"
+          />
+          <H2
+            className="!text-white  text-center mb-2 sm:mb-5 lg:mb-6 xl:mb-[76px]"
+            heading="Angebot"
+          />
+          <div className="flex flex-row -mx-3 pb-6 md:pb-16 lg:pb-[77px] justify-center flex-wrap ">
+            {howWeWorkCardlist.map((obj, index) => (
+              <div
+                className="lg:w-4/12 md:1/2 pt-4 md:pt-[22px] !px-[11.5px] sm:w-[49%] w-full"
+                key={index}
+              >
+                <Corporate
+                  svg={obj.svg}
+                  title={obj.title}
+                  description={obj.description}
+                  linkText={obj.link}
+                  arrow={<Arrow />}
+                  cardBox={index === 1 || index === 3 || index === 5}
+                />
+              </div>
+            ))}
+          </div>
+          <img
+            className="w-[90px] pointer-events-none h-[100px] md:w-[140px] md:h-[171px] mx-auto"
+            src={angebotLogo}
+            alt="logo_angebot"
+          />
         </div>
-        <img
-          className="w-[90px] pointer-events-none h-[100px] md:w-[140px] md:h-[171px] mx-auto"
-          src={angebotLogo}
-          alt="logo_angebot"
-        />
       </div>
-    </div>
+    </section>
   );
 };
 
